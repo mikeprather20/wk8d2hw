3d1e20c39a52a358af3fe0b2a537b54cebdc2e26
Michael Prather <mikeprather20@gmail.com>
2022-08-17 16:20:24 -0500

    second

--- src/ToDoList.js
+++ src/ToDoList.js
@@ -1,10 +1,14 @@
+
+
 import React, { Component } from 'react'
 
 export default class ToDoList extends Component {
   constructor() {
     super();
     this.state = {
-      todos: {done:"some string"}
+      todos: {
+        todo : []
+      }
     }
   };
 
@@ -33,7 +37,7 @@ export default class ToDoList extends Component {
     e.preventDefault()
     const userInput = e.target.userinput.value;
     console.log(userInput)
-    this.setState({ todos: userInput })
+    this.setState({ todos: this.todos.todo += userInput })
   };
 
 
@@ -44,7 +48,7 @@ export default class ToDoList extends Component {
 
         <form onSubmit={this.getInput}>
           <input name='userinput' placeholder='Your To Do Here' />
-          <button onClick={() => { this.setState({ done: true }) }}>Add to list</button>
+          <button className="btn btn-primary" type='submit'>Add to list</button>
           <a href="/ToDoList" className="btn btn-primary">Clear List</a>
         </form>
 
@@ -60,26 +64,67 @@ export default class ToDoList extends Component {
 };
 
 
+//SECOND WAY I TRIED
+
+// import React, { Component } from 'react'
+
+// export default class ToDoList extends Component {
+//   constructor() {
+//     super();
+//     this.state = {
+//       todos: []
+//     }
+//   };
+
+//   componentDidMount = async () => {
+//     this.getInput()
+//   };
+
+
+//   showCard = () => {
+//     this.state.todos.map((getInput) => {
+//       <div>
+//         <div className="card" style={{ width: "50rem" }}>
+//           <div className="card-body">
+//             <p className="card-text">{getInput.todos}</p>
+//             <input placeholder='Your To Do Here' />
+//             <button onClick={() => { this.setState({ done: true }) }}>Done</button>
+//             <a href="/Todos" className="btn btn-primary">Clear To Do</a>
+//           </div>
+//         </div>
+//       </div>
+//     })
+//   };
+
+//   getInput = async (input) => {
+//     this.setState({ todos:input })
+//   };
+
+//   search = (e)=>{
+//     e.preventDefault()
+//     const input = e.target.search.value;
+//     this.getInput(input)
+//   };
 
 
 
+//   render() {
+//     return (
+//       <div>
 
+//         <form onSubmit={this.getInput}>
+//           <input name='search' placeholder='Your To Do Here' />
+//           <button className="btn btn-primary" type='submit'>Add to list</button>
+//           <a href="/ToDoList" className="btn btn-primary">Clear List</a>
+//         </form>
 
+//         <div>
 
+//           {this.showCard()}
 
+//         </div>
 
-// getNews = async (input) => {
-//   const res = await fetch(`https://newsapi.org/v2/everything?q=${input}&apiKey=ace489dd71b74e8f9cf8aeedf4c0a864&pageSize=20`);
-//   const data = await res.json()
-//   console.log(data)
-//   this.setState({ articles: data.articles })
-// }
-
-// async componentDidMount() {
-//   this.getNews('bitcoin')
-// }
-// search = (e) => {
-//   e.preventDefault();
-//   const input = e.target.search.value;
-//   this.getNews(input)
+//       </div>
+//     )
+//   };
 // };
